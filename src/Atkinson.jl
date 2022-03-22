f57f6ca8ed723cf96c737fc20b1d84429417c79c
JosepER <jespasareig@gmail.com>
2022-03-22 17:29:07 +0100

    move checks_weights to a different script

--- src/Atkinson.jl
+++ src/Atkinson.jl
@@ -69,13 +69,6 @@ function atkinson(v::Array{<:Real,1}, w::Array{<:Real,1}, ϵ::Real)::Float64
 end
 
 
-function checks_weights(v::Array{<:Real,1}, w::Array{<:Real,1})
-    length(v) == length(w) ? nothing : throw(ArgumentError("`v` and `w` vectors must be the same size, got $(length(v)) and $(length(w))"))
-    any([isnan(x) for x in w]) ? throw(ArgumentError("`w` vector cannot contain NaN values")) : nothing
-    all(w .>= 0) ? nothing : throw(ArgumentError("`w` vector cannot contain negative entries"))
- end
- 
-
 @inline function norm_mean!(x::Array{<:Real,1})::Vector{Float64} 
     x = convert(Vector{Float64}, x)
     μx = Statistics.mean(x)
