ff019b7b3785fa8aff12a98c69d5afb923c5be4c
JosepER <jespasareig@gmail.com>
2022-03-22 16:54:36 +0100

    change the name of entropy function to gen_entropy

--- src/Entropy.jl
+++ src/Entropy.jl
@@ -1,23 +1,23 @@
 # Entropy.jl
 
-###### entropy #####
+###### generalized entropy #####
 """
-    entropy(v, α)
+    gen_entropy(v, α)
 
     Compute the Generalized Entropy Index of a vector `v` at a specified parameter `α`.
 
 # Examples
 ```jldoctest
-julia> entropy([8, 5, 1, 3, 5, 6, 7, 6, 3], 2)
+julia> gen_entropy([8, 5, 1, 3, 5, 6, 7, 6, 3], 2)
 0.09039256198347094
 ```
 """
-entropy(v::Array{<:Real,1}, α::Real)::Float64 = α == 1 ? theil(v) : (α == 0) ? (mld(v)) : (1/(α * (α - 1)) * sum(((v./Statistics.mean(v)).^α .- 1) * (1/length(v))) )
+gen_entropy(v::Array{<:Real,1}, α::Real)::Float64 = α == 1 ? theil(v) : (α == 0) ? (mld(v)) : (1/(α * (α - 1)) * sum(((v./Statistics.mean(v)).^α .- 1) * (1/length(v))) )
 
 
-###### weighted entropy #####
+###### weighted generalized entropy #####
 """
-    entropy(v, w, α)
+    gen_entropy(v, w, α)
 
 Compute the Generalized Entropy Index of a vector `v`, using weights given by a weight vector `w` 
 at a specified parameter `α`.
@@ -26,11 +26,11 @@ Weights must not be negative, missing or NaN. The weights and data vectors must 
 
 # Examples
 ```jldoctest
-julia> entropy([8, 5, 1, 3, 5, 6, 7, 6, 3], collect(0.1:0.1:0.9), 2)
+julia> gen_entropy([8, 5, 1, 3, 5, 6, 7, 6, 3], collect(0.1:0.1:0.9), 2)
 0.0709746654322026
 ```
 """
-function entropy(v::Array{<:Real,1},w::Array{<:Real,1}, α::Real)::Float64
+function gen_entropy(v::Array{<:Real,1},w::Array{<:Real,1}, α::Real)::Float64
 
     if α == 1
         return theil(v, w)
@@ -46,11 +46,11 @@ end
 
 
 """
-    wentropy(v, w, α)
+    wgen_entropy(v, w, α)
 
-Compute the Generalized Entropy Index of `v` with weights `w` at a specified parameter `α`. See also [`entropy`](@entropy)
+Compute the Generalized Entropy Index of `v` with weights `w` at a specified parameter `α`. See also [`gen_entropy`](gen_entropy)
 """
-wentropy(v::Array{<:Real,1},w::Array{<:Real,1}, α::Real)::Float64 = entropy(v,w,α)
+wgen_entropy(v::Array{<:Real,1},w::Array{<:Real,1}, α::Real)::Float64 = entropy(v,w,α)
 
 
 
